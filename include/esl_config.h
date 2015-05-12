5c53d4157eb711f7649f984b1e624fe00175ac12
Victor Torres <vpaivatorres@gmail.com>
2015-05-12 15:03:20 -0300

    Updating esl_config.h file.

--- include/esl_config.h
+++ include/esl_config.h
@@ -1,23 +1,23 @@
 /*
- * Copyright (c) 2007, Anthony Minessale II
+ * Copyright (c) 2007-2014, Anthony Minessale II
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 
+ *
  * * Neither the name of the original author; nor the names of any contributors
  * may be used to endorse or promote products derived from this software
  * without specific prior written permission.
- * 
- * 
+ *
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -35,11 +35,11 @@
  * @defgroup config Config File Parser
  * @ingroup config
  * This module implements a basic interface and file format parser
- * 
+ *
  * <pre>
  *
- * EXAMPLE 
- * 
+ * EXAMPLE
+ *
  * [category1]
  * var1 => val1
  * var2 => val2
@@ -79,53 +79,55 @@ extern "C" {
 /*!
   \brief Evaluate the truthfullness of a string expression
   \param expr a string expression
-  \return true or false 
+  \return true or false
 */
-#define esl_true(expr)\
-(expr && ( !strcasecmp(expr, "yes") ||\
-!strcasecmp(expr, "on") ||\
-!strcasecmp(expr, "true") ||\
-!strcasecmp(expr, "enabled") ||\
-!strcasecmp(expr, "active") ||\
-!strcasecmp(expr, "allow") ||\
-atoi(expr))) ? 1 : 0
+static __inline__ int esl_true(const char *expr) {
+  return (expr && (!strcasecmp(expr, "yes")
+           || !strcasecmp(expr, "on")
+           || !strcasecmp(expr, "true")
+           || !strcasecmp(expr, "enabled")
+           || !strcasecmp(expr, "active")
+           || !strcasecmp(expr, "allow")
+           || atoi(expr)));
+}
 
 /*!
   \brief Evaluate the falsefullness of a string expression
   \param expr a string expression
-  \return true or false 
+  \return true or false
 */
-#define esl_false(expr)\
-(expr && ( !strcasecmp(expr, "no") ||\
-!strcasecmp(expr, "off") ||\
-!strcasecmp(expr, "false") ||\
-!strcasecmp(expr, "disabled") ||\
-!strcasecmp(expr, "inactive") ||\
-!strcasecmp(expr, "disallow") ||\
-!atoi(expr))) ? 1 : 0
+static __inline__ int esl_false(const char *expr) {
+  return (expr && (!strcasecmp(expr, "no")
+           || !strcasecmp(expr, "off")
+           || !strcasecmp(expr, "false")
+           || !strcasecmp(expr, "disabled")
+           || !strcasecmp(expr, "inactive")
+           || !strcasecmp(expr, "disallow")
+           || !atoi(expr)));
+}
 
 typedef struct esl_config esl_config_t;
 
 /*! \brief A simple file handle representing an open configuration file **/
 struct esl_config {
-	/*! FILE stream buffer to the opened file */
-	FILE *file;
-	/*! path to the file */
-	char path[512];
-	/*! current category */
-	char category[256];
-	/*! current section */
-	char section[256];
-	/*! buffer of current line being read */
-	char buf[1024];
-	/*! current line number in file */
-	int lineno;
-	/*! current category number in file */
-	int catno;
-	/*! current section number in file */
-	int sectno;
+  /*! FILE stream buffer to the opened file */
+  FILE *file;
+  /*! path to the file */
+  char path[512];
+  /*! current category */
+  char category[256];
+  /*! current section */
+  char section[256];
+  /*! buffer of current line being read */
+  char buf[1024];
+  /*! current line number in file */
+  int lineno;
+  /*! current category number in file */
+  int catno;
+  /*! current section number in file */
+  int sectno;
 
-	int lockto;
+  int lockto;
 };
 
 /*!
@@ -174,5 +176,5 @@ ESL_DECLARE(int) esl_config_get_cas_bits(char *strvalue, unsigned char *outbits)
  * c-basic-offset:4
  * End:
  * For VIM:
- * vim:set softtabstop=4 shiftwidth=4 tabstop=4:
+ * vim:set softtabstop=4 shiftwidth=4 tabstop=4 noet:
  */
